4b72812e18ab78caed03fb6e4e29a6f56e2efa4f
mersad98 <mersadrezazadeh@gmail.com>
2024-01-04 19:44:46 +0330

    compelet dropdown menu

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,5 +1,41 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { AlignLeft } from "lucide-react";
+import { Button } from "./ui/button";
+import links from "@/utils/links";
+import Link from "next/link";
+
 function LinksDropdown() {
-  return <div></div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden "
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className="flex items-center gap-x-2">
+              {link.icon}
+              <span className="capitalize">{link.label}</span>
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
