59c7e9e88395e9e09ea27031500722e4e542f97f
mersad98 <mersadrezazadeh@gmail.com>
2024-01-04 10:58:09 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: { sans: ["var(--font-sans)"] },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
