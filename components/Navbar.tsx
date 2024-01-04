2a119e356b0aeaca9279b433b9bc85a3e27e69be
mersad98 <mersadrezazadeh@gmail.com>
2024-01-04 19:15:23 +0330

    compelet navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,19 @@
+import LinksDropdown from "./LinksDropdown";
+import SignOut from "./SignOut";
+import ThemeToggle from "./ThemeToggle";
+
 function Navbar() {
-  return <div></div>;
+  return (
+    <nav className="flex items-center justify-between bg-muted px-4 py-4 sm:px-16 lg:px-24">
+      <div>
+        <LinksDropdown />
+      </div>
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <SignOut />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
