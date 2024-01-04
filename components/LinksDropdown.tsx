ddbd02bd9a2dba13650b4f2b4eaf9d3e4ee465bc
mersad98 <mersadrezazadeh@gmail.com>
2024-01-04 19:46:59 +0330

    remove unnecessary imports

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -2,8 +2,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { AlignLeft } from "lucide-react";
