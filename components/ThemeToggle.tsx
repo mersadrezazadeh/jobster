20d996d739ea915ef77490b810afbb458e065fba
mersad98 <mersadrezazadeh@gmail.com>
2024-01-05 13:26:13 +0330

    clean up

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 
 function ThemeToggle() {
   const { setTheme } = useTheme();
