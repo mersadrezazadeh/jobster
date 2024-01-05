a9c6d8c946c3788d0c202c0c5da3c25ceacddb1f
mersad98 <mersadrezazadeh@gmail.com>
2024-01-05 13:08:18 +0330

    add theme provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,18 @@
 "use client";
 
+import ThemeProvider from "./theme-provider";
+
 function Providers({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
 }
 
 export default Providers;
