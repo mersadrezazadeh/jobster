884fe1bef22979231dc6ac894f2142fb0b5e606b
mersad98 <mersadrezazadeh@gmail.com>
2024-01-06 11:29:43 +0330

    complete job info

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -13,19 +13,35 @@ import { JobType } from "@/utils/types";
 import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
 import Link from "next/link";
 import DeleteJobButton from "./DeleteJobButton";
+import JobInfo from "./JobInfo";
 
 function JobCard({ job }: { job: JobType }) {
+  const { id, position, company, location, status, mode, created_at } = job;
+  const date = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    weekday: "short",
+  }).format(new Date(created_at));
+
   return (
     <Card className="bg-muted">
       <CardHeader>
-        <CardTitle>{job.position}</CardTitle>
-        <CardDescription>{job.company}</CardDescription>
+        <CardTitle>{position}</CardTitle>
+        <CardDescription>{company}</CardDescription>
       </CardHeader>
       <Separator />
-      <CardContent>INFO</CardContent>
+      <CardContent>
+        <JobInfo icon={<Briefcase className="size-4" />} text={mode} />
+        <JobInfo icon={<MapPin className="size-4" />} text={location} />
+        <JobInfo icon={<CalendarDays className="size-4" />} text={date} />
+        <Badge>
+          <JobInfo icon={<RadioTower className="size-4" />} text={status} />
+        </Badge>
+      </CardContent>
       <CardFooter className="flex gap-4">
         <Button asChild size="sm">
-          <Link href={`/jobs/${job.id}`}>edit</Link>
+          <Link href={`/jobs/${id}`}>edit</Link>
         </Button>
         <DeleteJobButton />
       </CardFooter>
--- components/JobInfo.tsx
+++ components/JobInfo.tsx
@@ -1,5 +1,10 @@
-function JobInfo() {
-  return <div></div>;
+function JobInfo({ icon, text }: { icon: React.ReactNode; text: string }) {
+  return (
+    <div className="flex items-center gap-x-2">
+      <span>{icon}</span>
+      <span>{text}</span>
+    </div>
+  );
 }
 
 export default JobInfo;
