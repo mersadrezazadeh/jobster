648129fc055c053374a6cf45cda172345bc37309
mersad98 <mersadrezazadeh@gmail.com>
2024-01-07 19:26:00 +0330

    clean up

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,5 +1,4 @@
 import CreateJobForm from "@/components/CreateJobForm";
-import SignOut from "@/components/SignOut";
 import { readUserSession } from "@/utils/actions";
 import { redirect } from "next/navigation";
 
@@ -8,11 +7,7 @@ async function AddJobPage() {
 
   if (!data.session) return redirect("/auth");
 
-  return (
-    <main>
-      <CreateJobForm />
-    </main>
-  );
+  return <CreateJobForm />;
 }
 
 export default AddJobPage;
