0e15f0c33e46d954c376b77dc88eee163bccdb37
mersad98 <mersadrezazadeh@gmail.com>
2024-01-07 17:08:17 +0330

    change page text

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,16 @@ export default function HomePage() {
       <header className="mx-auto max-w-6xl px-4 py-6 sm:px-8">
         <Image src={Logo} alt="Jobster Logo" />
       </header>
-      <section className="mx-auto -mt-20 grid h-screen max-w-6xl items-center gap-4 px-4 sm:px-8 md:grid-cols-[1fr,400px]">
+      <section className="mx-auto -mt-28 grid h-screen max-w-6xl items-center gap-4 px-4 sm:px-8 md:grid-cols-[1fr,400px]">
         <div>
-          <h1 className="text-4xl font-bold capitalize lg:text-7xl">
-            Job <span className="text-primary">Tracking</span>
+          <h1 className="whitespace-nowrap text-4xl font-bold capitalize lg:text-7xl">
+            Job <span className="text-primary">Tracking</span> app
           </h1>
           <p className="mt-4 max-w-md leading-loose">
-            I am baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
+            Your personal assistant for job search. Track job applications and
+            never miss an interview. Simplify your job hunt with Jobster.
           </p>
-          <Button asChild className="mt-4">
+          <Button asChild className="mt-4 font-semibold" size="lg">
             <Link href="/add-job">Get Started</Link>
           </Button>
         </div>
