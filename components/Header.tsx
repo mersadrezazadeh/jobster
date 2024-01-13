66961988c05fb381864c5672c99c2493c3ab7dcb
mersad98 <mersadrezazadeh@gmail.com>
2024-01-13 19:25:09 +0330

    fix header sahdow and toast

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./ThemeToggle";
 
 function Header() {
   return (
-    <header className="relative z-[9999] border border-b bg-muted px-4 py-4 shadow-2xl sm:px-16 lg:px-24">
+    <header className="relative z-[9999] border border-b bg-muted px-4 py-4 shadow-xl sm:px-16 lg:px-24">
       <div className="container flex items-center justify-between">
         <div>
           <LinksDropdown />
