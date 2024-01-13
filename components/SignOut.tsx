53ce2b1608f732a7f5f0fa346b8c0f94c05bf5f3
mersad98 <mersadrezazadeh@gmail.com>
2024-01-13 21:10:02 +0330

    fix logout

--- components/SignOut.tsx
+++ components/SignOut.tsx
@@ -1,12 +1,25 @@
-import { LogOut } from "lucide-react";
+"use client";
+
+import { Loader2, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { signOut } from "@/utils/actions";
+import { useTransition } from "react";
+import { cn } from "@/lib/utils";
 
 function SignOut() {
+  const [isPending, startTransition] = useTransition();
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    startTransition(async () => {
+      await signOut();
+    });
+  }
+
   return (
-    <form action={signOut}>
+    <form onSubmit={handleSubmit}>
       <Button variant="destructive">
-        <LogOut />
+        {!isPending ? <LogOut /> : <Loader2 className={cn("animate-spin")} />}
       </Button>
     </form>
   );
